e6a7ff3ec90cdba561087225dbd2610fd5902954
Hira_Nawaz <nawazhira48@gmail.com>
2025-05-26 17:17:17 +0500

    Create next_fit.cpp

---
+++ Memory Management/Memory Allocation Algorithms/next_fit.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int m, n;
+
+    cout << "Enter number of memory blocks: ";
+    cin >> m;
+    vector<int> blockSize(m);
+
+    cout << "Enter sizes of blocks:\n";
+    for (int i = 0; i < m; i++) {
+        cin >> blockSize[i];
+    }
+
+    cout << "Enter number of processes: ";
+    cin >> n;
+    vector<int> processSize(n);
+
+    cout << "Enter sizes of processes:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> processSize[i];
+    }
+
+    vector<int> allocation(n, -1);
+    vector<bool> blockAllocated(m, false);
+
+    int startIdx = 0;  // keeps track of where to start next search
+
+    // Next Fit Allocation
+    for (int i = 0; i < n; i++) {
+        int j = startIdx;
+        bool allocated = false;
+        do {
+            if (!blockAllocated[j] && blockSize[j] >= processSize[i]) {
+                allocation[i] = j;
+                blockAllocated[j] = true;
+                startIdx = (j + 1) % m;
+                allocated = true;
+                break;
+            }
+            j = (j + 1) % m;
+        } while (j != startIdx);
+
+        if (!allocated) {
+            allocation[i] = -1;  // Not allocated
+        }
+    }
+
+    cout << "\nNext Fit Allocation:\n";
+    cout << "Process No.\tProcess Size\tBlock No.\tWastage\n";
+
+    int totalWastage = 0;
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1) {
+            int wastage = blockSize[allocation[i]] - processSize[i];
+            totalWastage += wastage;
+            cout << i + 1 << "\t\t" << processSize[i] << "\t\t" << allocation[i] + 1 << "\t\t" << wastage << "\n";
+        } else {
+            cout << i + 1 << "\t\t" << processSize[i] << "\t\t" << "Not Allocated" << "\t" << "-" << "\n";
+        }
+    }
+
+    cout << "\nTotal wastage memory: " << totalWastage << "\n";
+
+    return 0;
+}
